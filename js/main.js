60bcafb400dfb36864ca347d8391d7f336e256b2
Maik Tappe <maik.tappe82@googlemail.com>
2026-03-21 19:06:52 +0100

    autoplay fullscreen

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 $("document").ready(function() {
+
+    const video = document.getElementById("myVideo");
+
+    video.addEventListener("click", () => {
+        if (video.requestFullscreen) {
+            video.requestFullscreen();
+        } else if (video.webkitRequestFullscreen) { // Safari
+            video.webkitRequestFullscreen();
+        } else if (video.msRequestFullscreen) { // IE
+            video.msRequestFullscreen();
+        }
+    });
+
 });
